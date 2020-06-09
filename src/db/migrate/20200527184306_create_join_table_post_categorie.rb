010b24cd610fee0a9f2a17dc334321bf5095a42d
Remi <remi.brat@outlook.fr>
2020-06-09 21:30:55 +0200

    mend

--- src/db/migrate/20200527184306_create_join_table_post_categorie.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTablePostCategorie < ActiveRecord::Migration[6.0]
-  def change
-    create_join_table :posts, :categories do |t|
-      t.index [:post_id, :category_id]
-      # t.index [:category_id, :post_id]
-    end
-  end
-end
